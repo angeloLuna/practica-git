5fb6bc592a503866468dcfae2df717723295bc86
mauricioislas <mauricio_2612@live.com.mx>
2017-06-12 02:06:58 -0500

    un nuevo complemento llamado inicio con ruta /discos, muestra los discos disponibles

---
+++ app/components/inicio/inicio.js
@@ -0,0 +1,19 @@
+(function(){
+  'use strict'
+
+  var inicio = {
+    controller: inicioCtrl,
+    templateUrl: "app/components/inicio/inicio.html"
+  }
+
+  angular
+    .module("Practica")
+    .component("componenteInicio", inicio)
+
+  function inicioCtrl(){
+
+    var inicio = this;
+
+
+  }
+})()
