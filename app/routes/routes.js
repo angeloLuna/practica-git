b8ec1a5bb627fb90b2fcd837b8de0acbd2826c9a
angel.luna.najera@gmail.com <angel.luna.najera@gmail.com>
2017-06-13 12:44:17 -0500

    rutas corregidas y resuelto el doble nav.js

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -7,35 +7,59 @@
 
   function config($routeProvider){
     $routeProvider
-      .when('/rap',
-      {
-        template:'<rap-component></rap-component>'
+      .when('/sugerencias',{
+        template: '<componente-inicio></componente-inicio>'
       })
-      .when("/rap/aleman",
-      {
-        template:"<aleman-component></aleman-component>"
+      .when('/',{
+        template: '<main-component></main-component>'
       })
-      .when("/rap/guadaloops",
-      {
-        template:"<guada-component></guada-component>"
+      .when('/genero',{
+        template: '<genero-component></genero-component>'
       })
-      .when("/rap/baston",
-      {
-        template:"<baston-component></baston-component>"
+      .when('/pate',{
+        template: '<pate-component></pate-component>'
       })
-      .when("/rap/simpson",
-      {
-        template:"<simpson-component></simpson-component>"
-      })
-      .when("/rap/zaque",
-      {
-        template:"<zaque-component></zaque-component>"
-      })
-      .when("/rap/tino",
-      {
-        template:"<tino-component></tino-component>"
-      })
-
+      .otherwise({ redirectTo: '/' })
+      .when('/paramore',{
+      template: "<practica-componentp></practica-componentp>"
+    })
+      .when('/muse',{
+      template: '<practica-component></practica-component>'
+    })
+      .when('/absolution',{
+      template: "<practica-absolution></practica-absolution>"
+    })
+    .when('/little',{
+      template: "<little-component></little-component>"
+    })
+    .when('/rap',
+    {
+      template:'<rap-component></rap-component>'
+    })
+    .when("/rap/aleman",
+    {
+      template:"<aleman-component></aleman-component>"
+    })
+    .when("/rap/guadaloops",
+    {
+      template:"<guada-component></guada-component>"
+    })
+    .when("/rap/baston",
+    {
+      template:"<baston-component></baston-component>"
+    })
+    .when("/rap/simpson",
+    {
+      template:"<simpson-component></simpson-component>"
+    })
+    .when("/rap/zaque",
+    {
+      template:"<zaque-component></zaque-component>"
+    })
+    .when("/rap/tino",
+    {
+      template:"<tino-component></tino-component>"
+    })
 
   }
 
